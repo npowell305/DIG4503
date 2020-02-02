e25789bb004c3814df1a388ec4bc3925f8ab8c13
npowell305 <33171464+npowell305@users.noreply.github.com>
2020-02-01 21:21:03 -0500

    bro

--- lab3/index.js
+++ lab3/index.js
@@ -4,7 +4,7 @@ const port = 80;
 
 
 App.use("/public", Express.static("public"));
-App.get("/public", function(req, res){
+App.get("/", function(req, res){
     res.send("Hello World!");
 });
 App.listen(port, ){
